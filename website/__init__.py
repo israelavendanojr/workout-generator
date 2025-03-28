76134d8769fef6d666bbae1b8d317a442fe5791e
israelavendanojr <93809935+israelavendanojr@users.noreply.github.com>
2025-03-27 23:00:22 -0700

    account functionality

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,7 +11,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'hjdaksdhjkasdhjklasdhjka'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
+    
     from .views import views
     from .auth import auth
     
@@ -18,15 +19,19 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     
     from .models import User, Note
+
     with app.app_context():
         db.create_all()
 
-    return app
+    
+    login_manager = LoginManager()
+    # where we send user if no user logged in
+    login_manager.login_view = 'auth.login' 
+    login_manager.init_app(app)
 
-# no longer neccesary? app.app_context() updated solution
-# def create_database(app):
-#     if not path.exists('website/' + DB_NAME):
-#         db.create_all()
-#         print('Created database')
-#     else:
-#         print('Database already exist, did not create database')
+    # find user by primary key (id)
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,39 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", user="Israel", boolean=True)
+    if request.method == 'POST':
+        print("here")
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                # if user valid and password valid, log in
+                login_user(user, remember=True)
+                flash('Logged in!', category='success')
+                home_page = redirect(url_for('views.home'))
+                return home_page
+            else:
+                flash('Incorrect password, try again', category='error')
+        else:
+            flash('Username not found', category='error')
+
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<h1>logout</h1>"
+    logout_user()
+    login_page = redirect(url_for('auth.login'))
+    return login_page
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -19,7 +44,10 @@ def sign_up():
         password2 = request.form.get('password2')
 
         # validate info from form
-        if len(username) < 3:
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already taken', category='error')
+        elif len(username) < 3:
             flash('Username must be 3 or more characters', category='error')
         elif len(password1) < 5:
             flash('Password must be greater than 5 or more characters', category='error')
@@ -27,9 +55,16 @@ def sign_up():
             flash('Passwords do not match', category='error')
         else:
             # add user to database
+            new_user = User(username=username, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+
+            # flash success message
             flash('Account successfully created!', category='success')
-            pass
+            # redirect to home page
+            home_page = redirect(url_for('views.home'))
+            return home_page
         
-    # elif request.method == 'GET':
-    #     pass
+
     return render_template("sign_up.html")

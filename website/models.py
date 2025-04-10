96810e83dc7dfd3f73a1524e0eef04fb7e003422
israelavendanojr <93809935+israelavendanojr@users.noreply.github.com>
2025-04-09 22:11:18 -0700

    password type len

--- website/models.py
+++ website/models.py
@@ -10,7 +10,7 @@ import enum
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(999))
 
     saved_plans = db.relationship('SavedPlan', backref='owner', lazy=True)
 
